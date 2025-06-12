023d46e1c0fd678ec381207f606540ae0ca3140d
Anil Kumar N <anilhn29@gmail.com>
2025-06-12 22:36:45 +0530

    11th Commit

---
+++ src/test/java/Excel/Demo1.java
@@ -0,0 +1,36 @@
+package Excel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Demo1 {
+public static void main(String[] args) throws IOException {
+		
+	FileInputStream file=new FileInputStream(System.getProperty("user.dir")+"\\TestData\\Myfile_Dynamic.xlsx");
+
+	XSSFWorkbook workbook=new XSSFWorkbook(file);
+	XSSFSheet sheet=workbook.getSheet("Dynamic Data");
+	
+	int rows=sheet.getLastRowNum();
+	int columns=sheet.getRow(0).getPhysicalNumberOfCells();
+	System.out.println("Number of Rows in Excel file :  "+rows);  
+	System.out.println("Number of cells in Excel file : "+columns);
+	
+	for(int r=1; r<rows; r++)
+	{
+		XSSFRow currentrow=sheet.getRow(r);
+		for(int c=0; c<columns; c++)
+		{
+			XSSFCell currentcell=currentrow.getCell(c);
+			System.out.println(currentcell.toString());
+		}
+	}
+	
+	}
+
+}
